6b31755797ced7fd0fed3cf6304b664d4af9248d
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-03-21 00:52:33 +0530

    Remove TagSelector component as it is no longer needed in the application. This cleanup helps streamline the codebase by eliminating unused files.

--- client/src/components/leetcode/TagSelector.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-import useProblems from "@/store/leetcode/hook";
-
-const TagSelector: React.FC = () => {
-  const { tags } = useProblems();
-};
-
-export default TagSelector;
